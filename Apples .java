99abc74e61dcaf707c1c57ff5a73794fed0703bc
genmayang <83032912+gengmayang75@users.noreply.github.com>
2021-10-03 06:27:21 -0700

    Create Apples .java

---
+++ Apples .java
@@ -0,0 +1,24 @@
+
+public class apples{
+  
+    public static void main(String args[]){
+      
+      int age;
+      age = 3;
+      
+      switch (age) {
+      case 1:
+          System.out.printIn("You can crawl");
+          break;
+       case 2:  
+           System.out.printIn("You can talk");
+          break;
+        case 3:  
+           System.out.printIn("You can get in toruble");
+          break;
+         default:
+           System.out.printIn("I don't know how old you are");
+         break;
+      }
+    }
+}
